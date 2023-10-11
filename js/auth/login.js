da9a63c83ecc991c80494d13c1694eaef5ac8348
Fredrick-GodAboveAll <fredrickmuasya553@gmail.com>
2023-10-11 22:33:02 +0300

    responsive and colored user

--- js/auth/login.js
+++ js/auth/login.js
@@ -1,65 +1,83 @@
 // This code handles user login and validation
 
-const userIDs = {
-    "fred": "1",
-    "muasya": "2",
-    "Dan": "3",
-    "emmanuel": "4"
-};
+async function fetchData() {
+    try {
+        const response = await fetch('helpers/fred.json');
 
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        return data;
+        
+    } catch (error) {
+        console.error('Error:',error);
+        return [];
+    }
+
+}
 
 
 // When the login form is submitted
-document.getElementById('login').addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent the default form submission behavior
+document.getElementById('login').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
 
     // Get the values of username, password, and userID from the form
-    const username = document.getElementById('me').value;
+    const nameInput = document.getElementById('me').value;
     const password = document.getElementById('you').value;
-    const userID = document.getElementById('userID').value;
+    const IdInput = document.getElementById('Id').value;
 
     // Open the IndexedDB database
     const request = indexedDB.open('UserDataDB', 1);
 
-    request.onerror = function(event) {
+    request.onerror = function (event)  {
         alert("IndexedDB error: " + event.target.errorCode);
     };
 
-    request.onsuccess = function(event) {
+    request.onsuccess = function (event)  {
         const db = event.target.result;
         const transaction = db.transaction(['users'], 'readonly');
         const objectStore = transaction.objectStore('users');
 
         // Check if the entered credentials match any user in UserDataDB
-        const getUserRequest = objectStore.get(username);
+        const getUserRequest = objectStore.get(nameInput);
 
-        getUserRequest.onsuccess = function(event) {
+        getUserRequest.onsuccess = async (event) =>  {
             const storedUser = event.target.result;
 
-            if (storedUser && storedUser.password === password) {
+            if (storedUser && storedUser.password == password) {
                 // If username and password match, proceed
                 // Check if the entered user ID matches the stored user ID
-                if (userIDs[username] === userID) {
+                
+                const data = await fetchData();
+                const match = data.find(person => person.Name === nameInput && person.Id == IdInput);
+                if (match) {
+                    const userId = match.Id;
+                    localStorage.setItem('loggedInUserId', userId)
                     // If the userID matches, it's a successful login
                     alert('Login successful!');
                     window.location.href = 'user.html';
                 } else {
                     // If the userID doesn't match, show an alert
-                    alert('User ID does not exist or is incorrect.');
+                    alert('User does not exist.');
                     window.location.reload();
                 }
             } else {
                 // If no user with matching credentials is found, show an alert
                 alert('Invalid username or password. Please try again.');
-                // Redirect to gitin.html or perform other actions as needed
+                // Redirect to gittin.html or perform other actions as needed
                 window.location.reload();
             }
+
         };
 
         transaction.oncomplete = function() {
             db.close();
         };
+
     };
+
 });
 
 
---
+++ js/using/display.js
@@ -0,0 +1,77 @@
+const loggedInUserId = parseInt (localStorage.getItem('loggedInUserId'));
+
+async function fetchUserData(userId) {
+    try {
+        const response = await fetch('helpers/fred.json');
+        if (!response.ok) {
+            throw new Error('failed to fetch data');
+        }
+
+        const data = await response.json();
+        const user = data.find(person => person.Id === userId);
+        return user;
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
+}
+
+fetchUserData(loggedInUserId)
+    .then(user => {
+        if (user) {
+            const userName = user.Name;
+            const contributions = user.Contributions;
+
+            const months = contributions.map(contribution => contribution.Month);
+            const amounts = contributions.map(contribution => contribution.Amount);
+
+            const ctx = document.getElementById('myChart').getContext('2d');
+
+            const myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: months,
+                    datasets: [{
+                        label: 'Amount',
+                        data: amounts,
+                        backgroundColor: [
+                            'rgba(255, 26, 104, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(0, 0, 0, 0.2)'
+                        ],
+                
+                        borderColor: [
+                            'rgba(255, 26, 104, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(0, 0, 0, 1)'
+                        ],
+                        
+                        borderWidth: 2,
+                        barPercentage: 0.6
+                    }]
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+
+            document.getElementById('userName').textContent = userName;
+
+        } else {
+            alert('User not found');
+        }
+
+    });
