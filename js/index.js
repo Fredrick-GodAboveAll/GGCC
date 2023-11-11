aafff7f713c82d33a110b23d88a01e39274f24b0
Fredrick-GodAboveAll <fredrickmuasya553@gmail.com>
2023-11-11 12:34:05 +0300

    advert

--- js/index.js
+++ js/index.js
@@ -75,3 +75,37 @@ const sendEmail = (e) => {
 
 contactForm.addEventListener('submit', sendEmail)
 
+
+
+// for the advert 
+
+const countDownData = new Date("Nov 14, 2023 8:00:00").getTime();
+
+const days = document.querySelector(".days");
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+
+const discountContainer = document.querySelector(".discount-container");
+
+const x = setInterval(function(){
+    const now = new Date().getTime();
+    const distance = countDownData - now;
+
+    let daysValue = Math.floor (distance / (1000 * 60 * 60 * 24)).toString().padStart(2, "0");
+    let hoursValue = Math.floor ((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, "0");
+    let minutesValue = Math.floor ((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, "0");
+    let secondsValue = Math.floor ((distance % (1000 * 60)) / 1000).toString().padStart(2, "0");
+
+    days.innerHTML = daysValue;
+    hours.innerHTML = hoursValue;
+    minutes.innerHTML = minutesValue;
+    seconds.innerHTML = secondsValue;
+
+    if (distance < 0) {
+        clearInterval(x);
+        discountContainer.remove();
+    }
+
+},1000)
+
